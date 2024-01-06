f55cbc77280434cdbf0419dd3a029f0d3d3c23a0
pranjal singh sengar <67635283+pranjalsinghsengar@users.noreply.github.com>
2024-01-06 18:35:48 +0530

    Updated

--- LognPage.js
+++ LognPage.js
@@ -10,31 +10,35 @@ import {
   Alert,
 } from 'react-native';
 import React, {useContext, useEffect, useState} from 'react';
-import {FIREBASE_AUTH} from './fireConfig';
+// import {
+//   getAuth,
+//   linkWithCredential,
+//   signInWithPopup,
+//   GoogleAuthProvider,
+// } from 'firebase/auth';
+
 // import {createNativeStackNavigator} from '@react-navigation/native-stack';
 // import {getAuth, createUserWithEmailAndPassword} from 'firebase/auth';
-// import {GoogleAuthProvider, signInWithPopup} from 'firebase/auth';
+
 import {
   GoogleSignin,
   GoogleSigninButton,
   GoogleOneTapSignIn,
   statusCodes,
 } from '@react-native-google-signin/google-signin';
+
 import {RuskContext} from './Context';
-import {signInWithPopup, GoogleAuthProvider} from 'firebase/auth';
+
 import auth from '@react-native-firebase/auth';
 import database from '@react-native-firebase/database';
+import FIREBASE_AUTH from './src/firebase/fireConfig';
 import {borderRadius} from '@mui/system';
-
-// import {signInWithPopup, GoogleAuthProvider} from 'firebase/auth';
-// import {signInWithPopup, GoogleAuthProvider} from 'firebase/auth';
+// import {GoogleAuthProvider, signInWithPopup} from 'firebase/auth';
 
 // GoogleSignin.configure({
 //   webClientId:
 //     '744554475013-iejli0h4t7qhllcrmvj7gofdlgbdtliq.apps.googleusercontent.com',
 // });
-
-// import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
 
 const LognPage = ({navigation}) => {
   // const Stack = createNativeStackNavigator();
@@ -48,128 +52,97 @@ const LognPage = ({navigation}) => {
     setUserUID,
   } = useContext(RuskContext);
 
-  const provider = new GoogleAuthProvider();
-
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   // const [userInfo, setUserInfo] = useState();
 
   useEffect(() => {
     GoogleSignin.configure({
+      scopes: ['https://www.googleapis.com/auth/contacts.readonly'],
       webClientId:
         '744554475013-iejli0h4t7qhllcrmvj7gofdlgbdtliq.apps.googleusercontent.com',
+      offlineAccess: true,
     });
   }, []);
 
-  // const handleGoogleSignIn = async () => {
-  //   await signInWithPopup(firebseAuth, provider)
-  //     .then(result => {
-  //       // This gives you a Google Access Token. You can use it to access the Google API.
-  //       const credential = GoogleAuthProvider.credentialFromResult(result);
-  //       const token = credential.accessToken;
-  //       // The signed-in user info.
-  //       const user = result.user;
-  //       if (user) {
-  //         setUserInfo(user);
-  //         navigation.navigate('Window');
-  //       }
-
-  //       // IdP data available using getAdditionalUserInfo(result)
-  //       // ...
-  //     })
-  //     .catch(error => {
-  //       // Handle Errors here.
-  //       const errorCode = error.code;
-  //       const errorMessage = error.message;
-  //       // The email of the user's account used.
-  //       const email = error.customData.email;
-  //       // The AuthCredential type that was used.
-  //       const credential = GoogleAuthProvider.credentialFromError(error);
-  //       // ...
-  //     });
-  // };
-
-  // const handle = () => {
-  //   navigation.navigate('Window');
-  // };
   const [loadingText, setloadingText] = useState(null);
 
   const handleGoogleSignIn = async () => {
     try {
       await GoogleSignin.hasPlayServices();
       const {idToken, ...Info} = await GoogleSignin.signIn(); // Use object destructuring to get idToken and the rest of the Info
-      console.log('Step 1 Done');
-      setloadingText('Fetching Your data...');
+      // console.log('Step 1 Done');
+      // setloadingText('Fetching Your data...');
 
-      // Create a Google credential with the token
+      // // Create a Google credential with the token
       const googleCredential = await auth.GoogleAuthProvider.credential(
         idToken,
       );
-      console.log('Step 2 Done');
+      // console.log('Step 2 Done');
 
-      // Sign-in the user with the credential
+      // // Sign-in the user with the credential
       const userCredential = await auth().signInWithCredential(
         googleCredential,
       );
-      console.log('Step 3 Done');
+      // console.log('Step 3 Done');
 
-      // Access the UID from the userCredential
+      // // Access the UID from the userCredential
       const uid = userCredential.user.uid;
-      console.log('Step 4 Done - UID', uid);
+      // console.log('Step 4 Done - UID', uid);
 
       const userSnapshot = await database().ref(`Users/${uid}`).once('value');
       const userExists = await userSnapshot.val();
       console.log('Step 5 Done 0- userExists', userExists);
+      Alert.alert('userExists');
+      // let userData;
+      // if (!userExists) {
+      //   console.log('userExists');
 
-      let userData;
-      if (!userExists) {
-        console.log('userExists');
+      //   await database().ref(`Users/${uid}`).set({
+      //     email: Info.user.email,
+      //     id: Info.user.id,
+      //     uid: uid,
+      //     givenName: Info.user.givenName,
+      //     familyName: Info.user.familyName,
+      //     photo: Info.user.photo,
+      //     name: Info.user.name,
+      //     inviteID: InviteId,
+      //   });
+      //   await database().ref(`Users/${uid}/Wallet`).set({
+      //     coins: coinWallet,
+      //   });
+      //   await database()
+      //     .ref(`Users/${uid}/InviteBy`)
+      //     .set({inviteStatus: false});
+      //   // Set userData for updating local state
+      //   userData = {
+      //     email: Info.user.email,
+      //     id: Info.user.id,
+      //     uid: uid,
+      //     givenName: Info.user.givenName,
+      //     familyName: Info.user.familyName,
+      //     photo: Info.user.photo,
+      //     name: Info.user.name,
+      //   };
+      // } else {
+      //   // If the user already exists, update the existing record
+      //   await database().ref(`Users/${uid}`).update({
+      //     email: Info.user.email,
+      //     id: Info.user.id,
+      //     uid: uid,
+      //     givenName: Info.user.givenName,
+      //     familyName: Info.user.familyName,
+      //     photo: Info.user.photo,
+      //     name: Info.user.name,
+      //     //   // inviteID: InviteId,
+      //   });
 
-        await database().ref(`Users/${uid}`).set({
-          email: Info.user.email,
-          id: Info.user.id,
-          uid: uid,
-          givenName: Info.user.givenName,
-          familyName: Info.user.familyName,
-          photo: Info.user.photo,
-          name: Info.user.name,
-          inviteID: InviteId,
-        });
-        await database().ref(`Users/${uid}/Wallet`).set({
-          coins: coinWallet,
-        });
-        await database()
-          .ref(`Users/${uid}/InviteBy`)
-          .set({inviteStatus: false});
-        // Set userData for updating local state
-        userData = {
-          email: Info.user.email,
-          id: Info.user.id,
-          uid: uid,
-          givenName: Info.user.givenName,
-          familyName: Info.user.familyName,
-          photo: Info.user.photo,
-          name: Info.user.name,
-        };
-      } else {
-        // If the user already exists, update the existing record
-        await database().ref(`Users/${uid}`).update({
-          email: Info.user.email,
-          id: Info.user.id,
-          uid: uid,
-          givenName: Info.user.givenName,
-          familyName: Info.user.familyName,
-          photo: Info.user.photo,
-          name: Info.user.name,
-          //   // inviteID: InviteId,
-        });
-
-        // Fetch the updated user data from the database
-        // userData = (await database().ref(`Users/${uid}`).once('value')).val();
-      }
-      setUserUID(uid);
-      console.log('Start Navigating...');
-      setloadingText('Almost Done');
+      //   // Fetch the updated user data from the database
+      //   // userData = (await database().ref(`Users/${uid}`).once('value')).val();
+      // }
+      // setUserUID(uid);
+      // console.log('Start Navigating...');
+      // setloadingText('Almost Done');
 
       // console.log('Google Sign-In successful');
       // console.log('googleCredential ', googleCredential);
@@ -192,7 +165,7 @@ const LognPage = ({navigation}) => {
         });
       }
     }
-    
+
     if (userUID) {
       await setloadingText('Done');
       setTimeout(() => {
@@ -203,9 +176,47 @@ const LognPage = ({navigation}) => {
     }
   };
 
-  // useEffect(() => {
-  //   navigation.navigate('Window');
-  // }, [userInfo]);
+  // const provider = new GoogleAuthProvider();
+  // const auth = FIREBASE_AUTH;
+  // const loginUsingWeb = async () => {
+  //   try {
+  //     // Step 1: User tries to sign in using Google.
+  //     let result = await signInWithPopup(getAuth(), new GoogleAuthProvider());
+  //   } catch (error) {
+  //     // Step 2: User's email already exists.
+  //     if (error.code === 'auth/account-exists-with-different-credential') {
+  //       // The pending Google credential.
+  //       let pendingCred = error.credential;
+
+  //       // Step 3: Save the pending credential in temporary storage,
+
+  //       // Step 4: Let the user know that they already have an account
+  //       // but with a different provider, and let them choose another
+  //       // sign-in method.
+  //     }
+  //   }
+
+  //   // ...
+
+  //   try {
+  //     // Step 5: Sign the user in using their chosen method.
+  //     let result = await signInWithPopup(getAuth(), userSelectedProvider);
+
+  //     // Step 6: Link to the Google credential.
+  //     // TODO: implement `retrievePendingCred` for your app.
+  //     let pendingCred = retrievePendingCred();
+
+  //     if (pendingCred !== null) {
+  //       // As you have access to the pending credential, you can directly call the
+  //       // link method.
+  //       let user = await linkWithCredential(result.user, pendingCred);
+  //     }
+
+  //     // Step 7: Continue to app.
+  //   } catch (error) {
+  //     // ...
+  //   }
+  // };
 
   return (
     <View
@@ -282,6 +293,7 @@ const LognPage = ({navigation}) => {
                   color={GoogleSigninButton.Color.Dark}
                   // backgroundColor={GoogleSigninButton.backgroundColor.Dark}
                   onPress={
+                    // () => handleGoogleSignIn()
                     () => handleGoogleSignIn()
                     // .then(() => setsuccessFully(true))
                   }
--- src/firebase/fireConfig.js
+++ src/firebase/fireConfig.js
@@ -1,12 +1,13 @@
 // Import the functions you need from the SDKs you need
 import {initializeApp} from 'firebase/app';
 import {getAnalytics} from 'firebase/analytics';
-// import {getAuth} from '@firebase/auth';
+import {getAuth} from 'firebase/auth';
 // import {getAuth} from 'firebase/auth';
 import {getFirestore} from 'firebase/firestore';
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
-import {initializeAuth, getReactNativePersistence} from 'firebase/auth';
+import {initializeAuth, getReactNativePersistence} from '@firebase/auth';
+
 import ReactNativeAsyncStorage from '@react-native-async-storage/async-storage';
 
 // Your web app's Firebase configuration
@@ -14,6 +15,7 @@ import ReactNativeAsyncStorage from '@react-native-async-storage/async-storage';
 const firebaseConfig = {
   apiKey: 'AIzaSyAxZr24IaJajYy5WHNCoNGfLqPjJBR6yok',
   authDomain: 'ruskrupees.firebaseapp.com',
+  databaseURL: 'https://ruskrupees-default-rtdb.firebaseio.com',
   projectId: 'ruskrupees',
   storageBucket: 'ruskrupees.appspot.com',
   messagingSenderId: '744554475013',
@@ -22,9 +24,10 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-export const fireApp = initializeApp(firebaseConfig);
-// export const analytics = getAnalytics(FIREBASE_APP);
-export const firebseAuth = initializeAuth(fireApp, {
-  persistence: getReactNativePersistence(ReactNativeAsyncStorage),
-});
-export const FIREBASE_DB = getFirestore(fireApp);
+export const FIREBASE_APP = initializeApp(firebaseConfig);
+export const analytics = getAnalytics(FIREBASE_APP);
+export const FIREBASE_AUTH = getAuth(FIREBASE_APP);
+// export const FIREBASE_AUTH = initializeAuth(FIREBASE_APP, {
+//   persistence: getReactNativePersistence(ReactNativeAsyncStorage),
+// });
+export const FIREBASE_DB = getFirestore(FIREBASE_APP);
